b5bdf5536870d2783ff4e5d1ebb8d33c7a2af23c
thecodedoctorr <dranotherone@gmail.com>
2022-06-20 13:02:31 +0200

    final tweekstest file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutterdex/main.dart';
+import 'package:flutterdeck/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
